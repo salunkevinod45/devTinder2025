fd733a5066d4238093adec717bad8373bf1ccf81
vinod salunke <salunkevinod45@gmail.com>
2025-10-10 21:25:36 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.use("/hello", (req, res) => {
+  res.send("Hello world");
+});
+
+app.use("/test",(req,res)=>{
+res.send('test path of node js server')
+})
+
+app.use("/", (req, res) => {
+  res.send("Dashboard of node js is first program");
+});
+
+
+
+app.listen(7777, () => {
+  console.log("server is running on port number 7777");
+});

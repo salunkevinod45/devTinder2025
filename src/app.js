15957fec2f710ef71a22cc557732c4c3a1a08691
vinod salunke <salunkevinod45@gmail.com>
2025-10-13 20:48:30 +0530

    node js routing

--- src/app.js
+++ src/app.js
@@ -2,21 +2,33 @@ const express = require("express");
 
 const app = express();
 
-
-
-app.use("/hello", (req, res) => {
-  res.send("Hello world");
+app.use("/test", (req, res) => {
+  res.send("test path of node js server");
 });
 
-app.use("/test",(req,res)=>{
-res.send('test path of node js server')
-})
-
-app.use("/", (req, res) => {
-  res.send("Dashboard of node js is first program");
+app.use("/user", (req, res) => {
+  res.send("this is sequence of router matters in node js");
 });
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Vinod", lastName: "Salunke" });
+});
 
+app.post("/user", (req, res) => {
+  res.send("data saved into the database successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("user data updated into the database successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("user name updated into the database successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted from the database successfully");
+});
 
 app.listen(7777, () => {
   console.log("server is running on port number 7777");
